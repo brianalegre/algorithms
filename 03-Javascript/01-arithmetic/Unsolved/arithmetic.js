a6a9d1ec5a22825fd21c4ed269970d4141e4d95e
brianalegre <brialegre@yahoo.com>
2022-07-17 15:06:05 -0700

    Added Week3 Algorithms

---
+++ 03-Javascript/01-arithmetic/Unsolved/arithmetic.js
@@ -0,0 +1,30 @@
+// Fill in the body of each function with the code required
+
+// Variables
+let num1 = 5;
+let num2 = 10;
+
+
+var add = function(num1, num2) {
+    return num1 + num2;
+
+};
+
+var subtract = function(num1, num2) {
+    return num1 - num2;
+};
+
+var multiply = function(num1, num2) {
+    return num1 * num2;
+
+};
+
+var divide = function(num1, num2) {
+    return num1 / num2;
+
+};
+
+// add(num1, num2);
+// subtract(num1, num2);
+// multiply(num1, num2);
+// divide(num1, num2);

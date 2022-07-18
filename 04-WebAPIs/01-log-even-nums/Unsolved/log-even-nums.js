e7eefdef828a7f92be3553c198f1a9da90d4bbd0
Brian Alegre <brialegre@yahoo.com>
2022-07-17 20:19:35 -0700

    Completed 04-WebAPIs Algorithms

--- 04-WebAPIs/01-log-even-nums/Unsolved/log-even-nums.js
+++ 04-WebAPIs/01-log-even-nums/Unsolved/log-even-nums.js
@@ -1,4 +1,11 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
-var logEvenNums = function(num) {};
+var logEvenNums = function (num) {
+    ;
+    // 0 + 2, until results = num
+
+    for (var i = 0; i <= num; i += 2) {
+        console.log(i);
+    }
+}
